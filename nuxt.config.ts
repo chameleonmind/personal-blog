fcbbb12d3e1100a6b6b2e2395c062e800188d217
Milos Milosevic <milos.milosevic@badin.rs>
2024-06-03 04:34:17 +0200

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,13 +27,19 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Nuxt Content Blog' }
+        { hid: 'description', name: 'description', content: 'Nuxt Content Blog' },
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' }
       ],
       htmlAttrs: {
         lang: 'en'
       },
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
       ]
     }
   }
