3ae439bba71a0df28eb46155f9fcadf25596c5c5
Milos Milosevic <highlander.mm@gmail.com>
2025-04-16 16:50:23 +0200

    Add article

---
+++ components/content/CallToComment.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p class="call-to-comment">
+    Want to talk about this post? Find me on
+    <CommonLinkOut href="https://twitter.com/chameleonmind">
+      Twitter
+    </CommonLinkOut>
+    or
+    <CommonLinkOut href="https://medium.com/@chameleonmind">
+      Medium
+    </CommonLinkOut>
+    .
+  </p>
+</template>
+
+<style scoped lang="scss">
+p.call-to-comment {
+  font-size: 1rem;
+  padding: 1rem;
+  border: 1px solid var(--border-color-alt);
+  border-radius: var(--border-radius);
+  background-color: var(--base-color-lighter);
+  margin: 0 auto 4rem;
+  width: fit-content;
+}
+</style>
